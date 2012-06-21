1c40c655fb995b69617fe770d3dad6a1530f47b5
aobrien <asobrien@gmail.com>
2012-06-21 11:44:55 -0400

    Fixed misaligned tab.
    Yaml file now correctly loaded into python.

---
+++ funcs.py
@@ -0,0 +1,9 @@
+# A bunch of functions that we'll need to deal with making an icartt header...
+# no organization for now!
+
+
+# Import the yaml
+import yaml
+
+f = open(fin, 'r')
+hdr = yaml.load(f)
